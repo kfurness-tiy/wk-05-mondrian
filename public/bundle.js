66aca3f270defe83836e5c1b782f7d3c7ecc0d86
kelly <kelly.n.waite@gmail.com>
2016-10-24 22:14:28 -0600

    WIP Pick a random div to change bgcolor of

--- public/bundle.js
+++ public/bundle.js
@@ -83,7 +83,7 @@
 
 
 	// module
-	exports.push([module.id, "body {\n  display: flex;\n  flex-direction: column;\n  align-items: center;\n  font-family: Helvetica, sans-serif; }\n\ndiv {\n  box-sizing: border-box; }\n\nbutton {\n  padding: 10px;\n  margin-top: 20px;\n  background-color: black;\n  color: white;\n  border-radius: 15px;\n  border: none;\n  font-size: 1.25em; }\n\n.parent {\n  display: flex;\n  flex-direction: column;\n  height: 500px;\n  width: 500px; }\n\n.row1 {\n  display: flex;\n  flex-direction: row;\n  justify-content: flex-start;\n  height: 33%;\n  border-bottom: 10px solid black; }\n  .row1 .left_col {\n    background-color: #ede2dd; }\n\n.row2 {\n  display: flex;\n  flex-direction: row;\n  justify-content: flex-start;\n  height: 33%;\n  border-bottom: 10px solid black; }\n\n.row3 {\n  display: flex;\n  flex-direction: row;\n  justify-content: flex-start;\n  height: 33%; }\n\n.row3_1 {\n  display: flex;\n  flex-direction: row;\n  justify-content: flex-start;\n  height: 100%; }\n\n.row3_2 {\n  display: flex;\n  flex-direction: column;\n  align-items: flex-start;\n  width: 30%;\n  height: 100%; }\n\n.row3_3 {\n  width: 30%;\n  height: 100%;\n  border-left: 10px solid black; }\n\n.left_col {\n  width: 40%;\n  height: 100%;\n  border-right: 10px solid black; }\n\n.right_col {\n  width: 60%;\n  height: 100%; }\n\n.col1a {\n  width: 25%;\n  height: 100%;\n  background-color: #1af312; }\n\n.col1b {\n  width: 75%;\n  height: 100%;\n  border-left: 10px solid black; }\n\n.col2a {\n  height: 80%;\n  width: 100%;\n  background-color: #2d4733; }\n\n.col2b {\n  height: 20%;\n  width: 100%;\n  border-top: 10px solid black; }\n", ""]);
+	exports.push([module.id, "body {\n  display: flex;\n  flex-direction: column;\n  align-items: center;\n  font-family: Helvetica, sans-serif; }\n\ndiv {\n  box-sizing: border-box; }\n\nbutton {\n  padding: 10px;\n  margin: 20px;\n  background-color: black;\n  color: white;\n  border-radius: 15px;\n  border: none;\n  font-size: 1.25em; }\n\ninput {\n  font-size: 1.2em;\n  margin-top: 10px; }\n\nul {\n  display: flex;\n  flex-direction: row;\n  justify-content: space-around;\n  width: 300px;\n  margin: 0; }\n  ul li {\n    padding: 2px; }\n\n.parent {\n  display: flex;\n  flex-direction: column;\n  height: 500px;\n  width: 500px; }\n\n.row1 {\n  display: flex;\n  flex-direction: row;\n  justify-content: flex-start;\n  height: 33%;\n  border-bottom: 10px solid black; }\n  .row1 .left_col {\n    background-color: #744661; }\n\n.row2 {\n  display: flex;\n  flex-direction: row;\n  justify-content: flex-start;\n  height: 33%;\n  border-bottom: 10px solid black; }\n\n.row3 {\n  display: flex;\n  flex-direction: row;\n  justify-content: flex-start;\n  height: 33%; }\n\n.row3_1 {\n  display: flex;\n  flex-direction: row;\n  justify-content: flex-start;\n  height: 100%; }\n\n.row3_2 {\n  display: flex;\n  flex-direction: column;\n  align-items: flex-start;\n  width: 30%;\n  height: 100%; }\n\n.row3_3 {\n  width: 30%;\n  height: 100%;\n  border-left: 10px solid black; }\n\n.left_col {\n  width: 40%;\n  height: 100%;\n  border-right: 10px solid black; }\n\n.right_col {\n  width: 60%;\n  height: 100%; }\n\n.col1a {\n  width: 25%;\n  height: 100%;\n  background-color: #cb7e5a; }\n\n.col1b {\n  width: 75%;\n  height: 100%;\n  border-left: 10px solid black; }\n\n.col2a {\n  height: 80%;\n  width: 100%;\n  background-color: #39eda2; }\n\n.col2b {\n  height: 20%;\n  width: 100%;\n  border-top: 10px solid black; }\n", ""]);
 
 	// exports
 
@@ -414,7 +414,63 @@
 	  return rgbColor;
 	}
 
+	function randomDiv () {
+	    return Math.floor(Math.random() * 8);
+	}
+
+	function userInput () {
+	  return document.getElementById('input').value
+	}
+
+	function chooseColor (input) {
+	  switch (input) {
+	    case 'red':
+	      return 'red';
+	      break;
+	    case 'orange':
+	      return 'orange';
+	      break;
+	    case 'yellow':
+	      return 'yellow';
+	      break;
+	    case 'green':
+	      return 'green';
+	      break;
+	    case 'blue':
+	      return 'blue';
+	      break;
+	    case 'purple':
+	      return 'purple';
+	      break;
+	    case 'black':
+	      return 'black';
+	      break;
+	    case 'brown':
+	      return 'brown';
+	      break;
+	    case 'gray':
+	      return 'gray';
+	      break;
+	    default:
+	      return 'Please choose a color from the list below'
+	  }
+	}
+
+	function toScreen(chosen) {
+	  console.log(chosen);
+	  // var num = randomDiv();
+	  document.getElementsByClassName('random')[2].style.backgroundColor = chosen;
+	}
+
+	function pickColor () {
+	  var input = userInput();
+	  var chosen = chooseColor(input);
+	  toScreen(chosen);
+	}
+
 	document.querySelector(".refresh").addEventListener('click', newColor);
+
+	document.querySelector(".pickColor").addEventListener('click', pickColor);
 
 
 /***/ }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,4 +12,60 @@ function color() {
   return rgbColor;
 }
 
+function randomDiv () {
+    return Math.floor(Math.random() * 8);
+}
+
+function userInput () {
+  return document.getElementById('input').value
+}
+
+function chooseColor (input) {
+  switch (input) {
+    case 'red':
+      return 'red';
+      break;
+    case 'orange':
+      return 'orange';
+      break;
+    case 'yellow':
+      return 'yellow';
+      break;
+    case 'green':
+      return 'green';
+      break;
+    case 'blue':
+      return 'blue';
+      break;
+    case 'purple':
+      return 'purple';
+      break;
+    case 'black':
+      return 'black';
+      break;
+    case 'brown':
+      return 'brown';
+      break;
+    case 'gray':
+      return 'gray';
+      break;
+    default:
+      return 'Please choose a color from the list below'
+  }
+}
+
+function toScreen(chosen) {
+  console.log(chosen);
+  // var num = randomDiv();
+  document.getElementsByClassName('random')[2].style.backgroundColor = chosen;
+}
+
+function pickColor () {
+  var input = userInput();
+  var chosen = chooseColor(input);
+  toScreen(chosen);
+}
+
 document.querySelector(".refresh").addEventListener('click', newColor);
+
+document.querySelector(".pickColor").addEventListener('click', pickColor);
